429a8ebd0a955435c0931eb8d06416400ba15359
harsha126 <harsha.sdm111@gmail.com>
2022-09-15 10:36:14 +0530

    Completed Assginment

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,58 @@
+package guru.springframework.domain;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private String description;
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
+    private BigDecimal amount;
+    @ManyToOne
+    private Recipe recipe;
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+}
